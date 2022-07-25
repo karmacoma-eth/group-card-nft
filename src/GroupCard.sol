ff1d944c343be806c38e27ca611fc4b0fcf7e847
karmacoma <karmacoma.eth@gmail.com>
2022-07-25 15:45:08 -0700

    Calling etch multiple times now updates your message
    Also:
    - no longer an `Owned` contract
    - finalized => isSealed

--- src/GroupCard.sol
+++ src/GroupCard.sol
@@ -3,16 +3,20 @@ pragma solidity ^0.8.13;
 
 import { Base64 } from "base64/base64.sol";
 import { ERC721 } from "solmate/tokens/ERC721.sol";
-import { Owned } from "solmate/auth/Owned.sol";
 import { Strings } from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 
-contract GroupCard is ERC721, Owned {
+contract GroupCard is ERC721 {
+
+    /*//////////////////////////////////////////////////////////////
+                        ERRORS / EVENTS / STRUCTS
+    //////////////////////////////////////////////////////////////*/
+
     error Unauthorized();
     error NameTooShort();
     error MessageTooShort();
     error TokenDoesNotExist(uint256 id);
-    error CardFinalized();
+    error CardSealed();
 
     event CardSigned(address from, string message, string signedBy);
 
@@ -21,17 +25,27 @@ contract GroupCard is ERC721, Owned {
         string signedBy;
     }
 
+
+    /*//////////////////////////////////////////////////////////////
+                                STORAGE
+    //////////////////////////////////////////////////////////////*/
+
     mapping (uint256 => mapping (address => bool)) public authorizedSigners;
-    mapping (uint256 => Message[]) public messages;
-    mapping (uint256 => bool) public finalized;
-    mapping (uint256 => string) public cardNames;
+    mapping (uint256 => address[]) public actualSigners;
+    mapping (uint256 => mapping (address => Message)) public messages;
+    mapping (uint256 => bool) public isSealed;
+
+    /// @dev do not use JSON characters in the card name, they are not escaped
+    mapping (uint256 => string) public cardName;
 
     uint256 currentId = 1;
 
-    constructor(address _owner)
-        Owned(_owner)
-        ERC721("GroupCard", "CARD")
-    { }
+
+    /*//////////////////////////////////////////////////////////////
+                        CONSTRUCTOR / MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    constructor() ERC721("GroupCard", "CARD") { }
 
     modifier onlyNFTOwner(uint256 id) {
         require(msg.sender == _ownerOf[id]);
@@ -47,15 +61,15 @@ contract GroupCard is ERC721, Owned {
 
 
     /*//////////////////////////////////////////////////////////////
-                        INTERNAL MINT/BURN LOGIC
+                            MINT/BURN LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function mint(string calldata cardName, address[] calldata signers) public returns (uint256 id) {
+    function mint(string calldata _cardName, address[] calldata signers) public returns (uint256 id) {
         id = currentId;
 
         _safeMint(msg.sender, id);
 
-        cardNames[id] = cardName;
+        cardName[id] = _cardName;
 
         uint signersLength = signers.length;
         for (uint256 i = 0; i < signersLength; ) {
@@ -83,30 +97,41 @@ contract GroupCard is ERC721, Owned {
         authorizedSigners[id][signer] = true;
     }
 
+    /// @notice Signs a card with the given message.
+    /// @param id The id of the card to sign.
+    /// @param message The message to leave on the card (60 characters max).
+    /// @param signedBy The name of the signer
     function etch(uint256 id, string calldata message, string calldata signedBy) public tokenExists(id) {
         if (msg.sender != _ownerOf[id] && !authorizedSigners[id][msg.sender]) {
             revert Unauthorized();
         }
 
-        if (finalized[id]) {
-            revert CardFinalized();
+        if (isSealed[id]) {
+            revert CardSealed();
         }
 
         if (bytes(signedBy).length == 0) {
             revert NameTooShort();
         }
 
+        // we don't enforce the 60 characters max because of Unicode shenanigans, this is a
         if (bytes(message).length < 3) {
             revert MessageTooShort();
         }
 
-        messages[id].push(Message(message, signedBy));
+        // if this is a message from a new signer, add it to the list of actual signers
+        if (bytes(messages[id][msg.sender].message).length == 0) {
+            actualSigners[id].push(msg.sender);
+        }
+
+        messages[id][msg.sender] = Message(message, signedBy);
+
 
         emit CardSigned(msg.sender, message, signedBy);
     }
 
-    function seal(uint256 id, bool _sealed) public onlyNFTOwner(id) {
-        finalized[id] = _sealed;
+    function seal(uint256 id) public onlyNFTOwner(id) {
+        isSealed[id] = true;
     }
 
 
@@ -115,17 +140,19 @@ contract GroupCard is ERC721, Owned {
     //////////////////////////////////////////////////////////////*/
 
     function tokenURI(uint256 id) public view override tokenExists(id) returns (string memory) {
-        Message[] storage _messages = messages[id];
-        uint length = _messages.length;
+        address[] storage signers = actualSigners[id];
+        uint length = signers.length;
 
         string memory output = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" />';
         string[] memory parts = new string[](5);
 
         for (uint i = 0; i < length; ) {
+            /// go over each actual signer, retrieve the message they left on this card
+            Message storage message = messages[id][signers[i]];
             parts[0] = string(abi.encodePacked('<text x="10" y="', Strings.toString((2 * i + 1) * 20), '" class="base">'));
-            parts[1] = _messages[i].message;
+            parts[1] = message.message;
             parts[2] = string(abi.encodePacked('</text><text x="10" y="', Strings.toString((2 * i + 2) * 20), '" class="base">'));
-            parts[3] = _messages[i].signedBy;
+            parts[3] = message.signedBy;
             parts[4] = '</text>';
 
             output = string(abi.encodePacked(output, parts[0], parts[1], parts[2], parts[3], parts[4]));
@@ -135,7 +162,7 @@ contract GroupCard is ERC721, Owned {
         }
         output = string(abi.encodePacked(output, '</svg>'));
 
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "', cardNames[id], unicode'", "description": "Group cards for all occasions, signed by your friends ♥️", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "', cardName[id], unicode'", "description": "Group cards for all occasions, signed by your friends ♥️", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
--- test/GroupCardTest.sol
+++ test/GroupCardTest.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import { GroupCard } from "src/GroupCard.sol";
+import {GroupCard} from "src/GroupCard.sol";
 
 contract GroupCardTest is Test {
     GroupCard internal groupCard;
@@ -19,7 +19,7 @@ contract GroupCardTest is Test {
     }
 
     function setUp() public {
-        groupCard = new GroupCard(address(this));
+        groupCard = new GroupCard();
 
         cardOwner = mkaddr("cardOwner");
         cardRecipient = mkaddr("cardRecipient");
@@ -39,12 +39,40 @@ contract GroupCardTest is Test {
         vm.prank(signer1);
         groupCard.etch(id, "Happy Birthday!", unicode"♥️ karma");
 
-
         vm.prank(signer2);
         groupCard.etch(id, "Remember to wear your sweater", unicode"👵 mom");
 
         vm.prank(cardOwner);
         groupCard.safeTransferFrom(cardOwner, cardRecipient, id);
+
+        assertEq(groupCard.actualSigners(id, 0), signer1);
+        assertEq(groupCard.actualSigners(id, 1), signer2);
+        // assertEq(groupCard.actualSigners(id, 2), address(0));
+
+        emit log(groupCard.tokenURI(id));
+    }
+
+        function testLongMessage() public {
+        address[] memory signers = new address[](4);
+        signers[0] = mkaddr("karma1");
+        signers[1] = mkaddr("karma2");
+        signers[2] = mkaddr("karma3");
+        signers[3] = mkaddr("karma4");
+
+        vm.prank(cardOwner);
+        uint256 id = groupCard.mint("card name", signers);
+
+        vm.prank(mkaddr("karma1"));
+        groupCard.etch(id, "this is a message with 50 characters this is a mes", unicode"♥️ karma1");
+
+        vm.prank(mkaddr("karma2"));
+        groupCard.etch(id, "this is a message with 64 characters this is a message with 64 c", unicode"♥️ karma2");
+
+        vm.prank(mkaddr("karma3"));
+        groupCard.etch(id, "this is a message with 75 characters this is a message with 75 characters t", unicode"♥️ karma3");
+
+        vm.prank(mkaddr("karma4"));
+        groupCard.etch(id, "this is a message with 100 characters this is a message with 100 characters this is a message with 1", unicode"♥️ karma4");
 
         emit log(groupCard.tokenURI(id));
     }
